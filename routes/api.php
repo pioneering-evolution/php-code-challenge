c7286a06acceb7ae4b6e52e89b79fcbe146f3501
Anthony Farr <farr433@bellsouth.net>
2021-09-22 11:09:55 -0400

    Routes and Controllers
    Setting up the initial routes and controllers. Performer and Task APIs

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/performers', [\App\Http\Controllers\Api\PerformerController::class, 'index']);
+Route::get('/tasks/{id}', [\App\Http\Controllers\Api\TaskController::class, 'performerTasks']);
+Route::get('/tasks/', [\App\Http\Controllers\Api\TaskController::class, 'index']);
